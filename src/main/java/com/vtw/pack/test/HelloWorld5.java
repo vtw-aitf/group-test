e12912995959936a7f9875dc83e5ccd6f3b588f8
mskim <moonsuk.kim@vtw.co.kr>
2026-02-09 13:19:08 +0900

    github username 변경 테스트 - package test포함

---
+++ src/main/java/com/vtw/pack/test/HelloWorld5.java
@@ -0,0 +1,9 @@
+package com.vtw.pack.test;
+
+public class HelloWorld5 {
+    public static void main(String[] args) {
+        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
+        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
+        System.out.print("Hello and welcome! 5555555");
+    }
+}
